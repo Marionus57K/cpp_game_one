aaf6fe0ec0ad07168eb92b0741e8099c73c870ca
Mairis Čeksters <mairis.ceksters@gmail.com>
2021-10-29 15:03:22 +0300

    might as well push this - nothing important

--- cpp_game_one/cpp_game_one.cpp
+++ cpp_game_one/cpp_game_one.cpp
@@ -1,6 +1,23 @@
 #include <iostream>
 
+void voidFunction(int&) {
+
+}
+
+int returnSumOfNumbers(int a, int b) {
+	return a + b;
+}
+
 int main() {
-	std::cout << "LoLXD";
+	int input{};
+
+	std::cout << "Please enter an integer: ";
+	std::cin >> input;
+
+	int result{};
+	result = returnSumOfNumbers(input, input + 3);
+
+	std::cout << "The double of " << input << " is " << input * 2 << "\n";
+	std::cout << "The triple of" << input << " is " << input * 3 << "\n";
 	return 0;
 }
